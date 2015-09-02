6b2d43a8c7fd60a94cfbfeaa38757dbe864f45b3
fechbern <bernd.fecht@encad-consulting.de>
2015-09-02 10:41:14 +0200

    finalized the color picker.

--- includes/theme-options.php
+++ includes/theme-options.php
@@ -18,12 +18,11 @@ class encadTemplateOptions
     /**
      * Add options page
      */
-    public function add_plugin_page()
-    {
+    public function add_plugin_page() {
         // This page will be under "Settings"
         add_options_page(
             'Settings Admin', 
-            'Theme Optionen', 
+            'Theme Options', 
             'manage_options', 
             'options-page', 
             array( $this, 'create_encad_template_options_page' )
@@ -33,14 +32,14 @@ class encadTemplateOptions
     /**
      * Options page callback
      */
-    public function create_encad_template_options_page()
-    {
+    public function create_encad_template_options_page() {
         // Set class property
-        $this->options = get_option( 'options' );
+        $this->options = get_option( 'encad_options' );
         ?>
         <div class="wrap">
-            <?php screen_icon(); ?><h2>Theme Einstellungen für <?php bloginfo('name'); ?></h2>           
-            <form method="post" action="theme-options.php">
+            <?php screen_icon(); ?><h2>Theme Options for <?php bloginfo('name'); ?></h2>   
+			<h3><small>by Bernd Fecht, encad consulting GmbH</small></h3>
+            <form method="post" action="options.php">
             <?php
                 // This prints out all hidden setting fields
                 settings_fields( 'encad_option_group' );   
@@ -58,34 +57,35 @@ class encadTemplateOptions
     public function page_init() {        
         register_setting(
             'encad_option_group', // Option group
-            'options', // Option name
+            'encad_options', // Option name
             array( $this, 'sanitize' ) // Sanitize
         );
 
         add_settings_section(
-            'headerSection', // ID
-            'encad Theme Optionen', // Title
-            array( $this, 'printSectionInfo' ), // Callback
+            'header_section', // ID
+            'Header', // Title
+            array( $this, 'print_section_info' ), // Callback
             'options-page' // Page
         );  
 		
 		add_settings_field (
-			'color-picker', // ID
-			'Header Color', // Title
-			array( $this, 'createColorSection'), // Callback
+			'header_color', // ID
+			'Header Hintergrundfarbe', // Title
+			array( $this, 'create_header_color_field'), // Callback
 			'options-page',
-			'headerSection' // Section
+			'header_section' // Section
 		);
 		
 		/**
-
-        add_settings_field(
+		
+        add_settings_field (
             'id_number', // ID
             'ID Number', // Title 
             array( $this, 'id_number_callback' ), // Callback
             'options-page', // Page
-            'setting_section_id' // Section           
-        );      
+            'header_section' // Section           
+        ); 
+
 
         add_settings_field(
             'title', 
@@ -96,33 +96,55 @@ class encadTemplateOptions
         );    
 		*/
     }
+	
+	 /**
+     * Sanitize each setting field as needed
+     *
+     * @param array $input Contains all settings fields as array keys
+     */
+    public function sanitize( $input )
+    {
+        $new_input = array();
+        if( isset( $input['id_number'] ) )
+            $new_input['id_number'] = absint( $input['id_number'] );
+
+        if( isset( $input['title'] ) )
+            $new_input['title'] = sanitize_text_field( $input['title'] );
+		
+		if( isset( $input['header_color'] ) )
+            $new_input['header_color'] = sanitize_text_field( $input['header_color'] );
+
+        return $new_input;
+    }
+
 
     /** 
      * Print the Section text
      */
-    public function printSectionInfo() {
-        print 'Geben Sie Ihre Einstellungen für das Template der encad consulting GmbH hier ein:';
+    public function print_section_info() {
+        print 'Enter your settings below:';
     }
 	
     /** 
      * Get the settings option array and print one of its values
      */
-    public function id_number_callback()
-    {
+    public function id_number_callback() {
         printf(
-            '<input type="text" id="id_number" name="options[id_number]" value="%s" />',
+            '<input type="text" id="id_number" name="encad_options[id_number]" value="%s" />',
             isset( $this->options['id_number'] ) ? esc_attr( $this->options['id_number']) : ''
         );
     }
 	
-	
-	public function createColorSection() {
-		?>
-		Color: <input type="text" class="color-picker" name="options[color-picker]" id='color-picker' value="#cc0000" />
-		<?php
-	}
-	
-	
+	/** 
+     * Create a color-picker field
+     */
+	public function create_header_color_field() {
+		printf(
+			'<input type="text" class="color-picker" name="encad_options[header_color]" id="header_color" value="%s" style="background:%s" />',
+			isset( $this->options['header_color'] ) ? esc_attr( $this->options['header_color']) : '#ffffff',
+			isset( $this->options['header_color'] ) ? esc_attr( $this->options['header_color']) : '#ffffff'
+		);		
+	}	
 
     /** 
      * Get the settings option array and print one of its values
--- js/iris-init.js
+++ js/iris-init.js
@@ -1,18 +1,24 @@
 jQuery(document).ready(function($){
-	 $('#color-picker').iris({
-	 width: 400,
-	 hide: true,
-	 palettes: true,
-	 change: function(event, ui) {
-	 // event = standard jQuery event, produced by whichever control was changed.
-	 // ui = standard jQuery UI object, with a color member containing a Color.js object
-	// change the headline color
-	 $("#color-picker").css( 'background', ui.color.toString());
-	 }
-
-	 });
-
-	 $(this).click(function() {
-	 $(this).iris('toggle'); //click came from somewhere else
-	});
+	$('.color-picker').iris({
+		width: 400,
+		hide: true,
+		palettes: true,
+		change: function(event, ui) {
+			// event = standard jQuery event, produced by whichever control was changed.
+			// ui = standard jQuery UI object, with a color member containing a Color.js object
+			// change the headline color
+			 $(".color-picker").css( 'background', ui.color.toString());
+		}
+	 });   
+	 
+    $(document).click(function (e) {
+        if (!$(e.target).is(".color-picker, .iris-picker, .iris-picker-inner")) {
+            $('.color-picker').iris('hide');
+        }
+    });
+	
+    $('.color-picker').click(function (event) {
+        $('.color-picker').iris('hide');
+        $(this).iris('show');
+    });
 });
